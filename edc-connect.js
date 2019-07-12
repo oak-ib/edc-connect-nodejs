f0fb6f8f65d2d934fd95b3660d6dd2df7d14bf74
Oak Isarapong <ozaaaaa@gmail.com>
2019-07-12 07:33:40 +0700

    add before build win app

---
+++ edc-connect.js
@@ -0,0 +1,88 @@
+const serialport = require("serialport");
+const createTable = require("data-table");
+const log = require("./logger");
+const nconf = require("nconf");
+nconf
+  .argv()
+  .env()
+  .file({ file: "config.json" });
+
+document.getElementById("edc-start").addEventListener("click", function(e) {
+    connectEdc();
+});
+
+var connectEdc = function() {
+    const port = new serialport("/dev/tty.ENDOSLP11-JL_SPP-3", {
+        baudRate: 9600,
+        parity: "none",
+        stopBits: 1,
+        dataBits: 8
+      });
+      
+      port.on("error", function(err) {
+        log.error(err.message);
+        console.log("Error: ", err.message);
+        reconnectEdc();
+      });
+
+      port.on("close", function() {
+        log.info("Close Connect");
+        reconnectEdc();
+      });
+      
+      //write message string
+      port.write("main screen turn on", function(err) {
+        if (err) {
+          log.error(err.message);
+          return console.log("Error on write: ", err.message);
+        }
+      
+        log.info("send complte");
+        console.log("message written");
+      });
+      
+      //write message Buffer
+      var buffer = new Buffer([0x02, 0x00, 0x35, 0x30, 0x30, 0x30, 0x30, 0x30, 0x30, 0x30, 0x30, 0x30,0x30, 0x31, 0x30, 
+          0x31, 0x31, //31,31 read card | 32,36 cancel read card | 39,32 re-print
+          0x30,0x30,0x30, 0x1C, 0x34, 0x30, 0x00, 0x12, 
+          0x30, 0x30, 0x30, 0x30, 0x30, 0x30, 0x30, 0x30, 0x35, 0x30, 0x30, 0x30, 0x1C, //price 12 digits , 1C end line no price
+          0x03, 0x14 ]);
+      //ทดสอบส่งเฉยๆ, ส่งเป็นtextให้ encode hex
+      port.write(buffer, function(err) {
+          if (err) {
+            log.error(err.message);
+            return console.log("Error on write: ", err.message);
+          }
+        
+          log.info("buffer send complte");
+          console.log("buffer message written");
+      });
+        
+      //read data
+    1 // Switches the port into "flowing mode"
+      port.on("data", function(data) {
+          log.data_receive(data);
+          console.log("Data:", data);
+          checkReceiveBuffer(data);
+      });
+}
+
+function checkReceiveBuffer(data){
+    if(data.length < 2){
+        return;
+    }
+
+    if( data ) {
+
+    }
+}
+
+
+// check for connection errors or drops and reconnect 2 sec
+var reconnectEdc = function () {
+    console.log('Initiating reconnect');
+    setTimeout(function(){
+        log.info('reconnect');
+        connectEdc();
+    }, 2000);
+  };
--- renderer.js
+++ renderer.js
@@ -1,28 +1,38 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// All of the Node.js APIs are available in this process.
+const serialport = require("serialport");
+const createTable = require("data-table");
+const log = require("./logger");
+const nconf = require("nconf");
+nconf
+  .argv()
+  .env()
+  .file({ file: "config.json" });
 
-const serialport = require('serialport')
-const createTable = require('data-table')
+nconf.set("portName", "COM6");
+console.log(nconf.get("portName"));
 
 serialport.list((err, ports) => {
-  console.log('ports', ports);
+  console.log("ports", ports);
   if (err) {
-    document.getElementById('error').textContent = err.message
-    return
+    log.info(err.message);
+    document.getElementById("error").textContent = err.message;
+    return;
   } else {
-    document.getElementById('error').textContent = ''
+    document.getElementById("error").textContent = "";
   }
 
   if (ports.length === 0) {
-    document.getElementById('error').textContent = 'No ports discovered'
+    log.info("No ports discovered");
+    document.getElementById("error").textContent = "No ports discovered";
   }
 
-  const headers = Object.keys(ports[0])
-  const table = createTable(headers)
-  tableHTML = ''
-  table.on('data', data => tableHTML += data)
-  table.on('end', () => document.getElementById('ports').innerHTML = tableHTML)
-  ports.forEach(port => table.write(port))
+  const headers = Object.keys(ports[0]);
+  const table = createTable(headers);
+  tableHTML = "";
+  table.on("data", data => (tableHTML += data));
+  table.on(
+    "end",
+    () => (document.getElementById("ports").innerHTML = tableHTML)
+  );
+  ports.forEach(port => table.write(port));
   table.end();
-})
+});
